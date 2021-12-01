48d61d5d3b1fafce0010d238c7ef3e4d8725ab8e
Him188 <Him188@mamoe.net>
2021-12-01 20:21:42 +0000

    Add runtime `dynamicDelegation`

---
+++ runtime/src/commonMain/kotlin/dynamicDelegationJvm.kt
@@ -0,0 +1,15 @@
+package me.him188.kotlin.dynamic.delgation
+
+/**
+ * Creates a dynamic delegation.
+ *
+ * Example of usage:
+ * ```
+ * public interface PluginManager {
+ *    public fun loadPlugin(plugin: Plugin)
+ *
+ *    public companion object INSTANCE : PluginManager by (dynamicDelegation { getInstanceFromOtherPlaces() })
+ * }
+ * ```
+ */
+public fun <R> dynamicDelegation(value: () -> R): R = throw NotImplementedError("Implemented as intrinsic")
