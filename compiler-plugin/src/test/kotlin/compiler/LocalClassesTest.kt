da3b8d705dba763dd84c9f004e50a529c07344ad
Him188 <Him188@mamoe.net>
2021-12-02 19:40:46 +0000

    Add LocalClassesTest

--- compiler-plugin/src/test/kotlin/compiler/LocalClassesTest.kt
+++ compiler-plugin/src/test/kotlin/compiler/LocalClassesTest.kt
@@ -1,5 +1,68 @@
 package compiler
 
-internal class LocalClassesTest : AbstractCompilerTest() {
+import createInstance
+import org.junit.jupiter.api.Test
+import runFunction
+import testJvmCompile
 
+internal class LocalClassesTest : AbstractCompilerTest() {
+    @Test
+    fun `by lambda`() = testJvmCompile(
+        """
+            interface TestClass {
+                fun getResult(): Int
+            }
+            object FileClass {
+            
+                fun fn() {
+                    class TestObject : TestClass by (dynamicDelegation { getInstanceFromOtherPlaces() })
+            
+                    TestObject().run { 
+                
+                        assertEquals(0, getResult())
+                        assertEquals(1, getResult())
+                    }
+                }
+            }
+            var called = 0
+            fun getInstanceFromOtherPlaces():TestClass  {
+                val v = called++
+                return object : TestClass {
+                    override fun getResult(): Int = v
+                }
+            }
+        """.trimIndent()
+    ) {
+        classLoader.loadClass("FileClass").createInstance().runFunction<Unit>("fn")
+    }
+
+    @Test
+    fun `by local fun`() = testJvmCompile(
+        """
+            interface TestClass {
+                fun getResult(): Int
+            }
+            object FileClass {
+            
+                fun fn() {
+                    var called = 0
+                    fun getInstanceFromOtherPlaces():TestClass  {
+                        val v = called++
+                        return object : TestClass {
+                            override fun getResult(): Int = v
+                        }
+                    }
+                    class TestObject : TestClass by (dynamicDelegation { getInstanceFromOtherPlaces() })
+            
+                    TestObject().run { 
+                
+                        assertEquals(0, getResult())
+                        assertEquals(1, getResult())
+                    }
+                }
+            }
+        """.trimIndent()
+    ) {
+        classLoader.loadClass("FileClass").createInstance().runFunction<Unit>("fn")
+    }
 }
