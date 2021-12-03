22442f13484bc0c578032cfaded0a1a6a8c20ca7
Him188 <Him188@mamoe.net>
2021-12-03 01:32:57 +0000

    Remove useless plugin options

--- compiler-plugin/src/main/kotlin/DynamicDelegationCommandLineProcessor.kt
+++ compiler-plugin/src/main/kotlin/DynamicDelegationCommandLineProcessor.kt
@@ -4,7 +4,6 @@ import com.google.auto.service.AutoService
 import me.him188.kotlin.dynamic.delegation.compiler.PluginCompilerConfigurationKeys.EXAMPLE
 import org.jetbrains.kotlin.compiler.plugin.AbstractCliOption
 import org.jetbrains.kotlin.compiler.plugin.CliOption
-import org.jetbrains.kotlin.compiler.plugin.CliOptionProcessingException
 import org.jetbrains.kotlin.compiler.plugin.CommandLineProcessor
 import org.jetbrains.kotlin.config.CompilerConfiguration
 
@@ -16,17 +15,18 @@ class DynamicDelegationCommandLineProcessor : CommandLineProcessor {
         val OPTION_EXAMPLE: CliOption = CliOption(
             EXAMPLE.name,
             "<example/example>",
-            "Example key"
+            "Example key",
+            required = false
         )
     }
 
     override val pluginId: String = COMPILER_PLUGIN_ID
-    override val pluginOptions: Collection<CliOption> = listOf(OPTION_EXAMPLE)
+    override val pluginOptions: Collection<CliOption> = listOf()
 
     override fun processOption(option: AbstractCliOption, value: String, configuration: CompilerConfiguration) {
-        when (option) {
-            OPTION_EXAMPLE -> configuration.put(EXAMPLE, value)
-            else -> throw CliOptionProcessingException("Unknown option: ${option.optionName}")
-        }
+//        when (option) {
+//            OPTION_EXAMPLE -> configuration.put(EXAMPLE, value)
+//            else -> throw CliOptionProcessingException("Unknown option: ${option.optionName}")
+//        }
     }
 }
