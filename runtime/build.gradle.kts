63e2a1990200bb02f4e4a8d8744df4d6fd73dda7
Him188 <Him188@mamoe.net>
2021-12-15 22:26:34 +0000

    Support all native targets

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -2,7 +2,6 @@
 
 import org.apache.tools.ant.taskdefs.condition.Os
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
-import org.jetbrains.kotlin.gradle.plugin.KotlinTargetPreset
 
 plugins {
     id("me.him188.maven-central-publish")
@@ -25,22 +24,31 @@ kotlin {
     val nativeMainSets = mutableListOf<KotlinSourceSet>()
     val nativeTestSets = mutableListOf<KotlinSourceSet>()
 
-    val addTarget = { preset: KotlinTargetPreset<*> ->
-        val target = targetFromPreset(preset, preset.name)
-        nativeMainSets.add(target.compilations["main"].kotlinSourceSets.first())
-        nativeTestSets.add(target.compilations["test"].kotlinSourceSets.first())
-    }
-
     if (ideaActive) {
         when {
+            // should use macosArm64 after https://github.com/gmazzo/gradle-buildconfig-plugin/issues/23 is fixed.
             Os.isFamily(Os.FAMILY_MAC) -> if (Os.isArch("aarch64")) macosX64("native") else macosX64("native")
             Os.isFamily(Os.FAMILY_WINDOWS) -> mingwX64("native")
             else -> linuxX64("native")
         }
     } else {
-        presets.forEach { preset ->
-            addTarget(preset)
-        }
+        // 1.6.0
+        val nativeTargets = arrayOf(
+            "androidNativeArm32, androidNativeArm64, androidNativeX86, androidNativeX64",
+            "iosArm32, iosArm64, iosX64, iosSimulatorArm64",
+            "watchosArm32, watchosArm64, watchosX86, watchosX64, watchosSimulatorArm64",
+            "tvosArm64, tvosX64, tvosSimulatorArm64",
+            "macosX64, macosArm64",
+            "linuxArm64, linuxArm32Hfp, linuxMips32, linuxMipsel32, linuxX64",
+            "mingwX64, mingwX86",
+            "wasm32"
+        ).flatMap { it.split(", ") }
+        presets.filter { it.name in nativeTargets }
+            .forEach { preset ->
+                val target = targetFromPreset(preset, preset.name)
+                nativeMainSets.add(target.compilations[org.jetbrains.kotlin.gradle.plugin.KotlinCompilation.MAIN_COMPILATION_NAME].kotlinSourceSets.first())
+                nativeTestSets.add(target.compilations[org.jetbrains.kotlin.gradle.plugin.KotlinCompilation.TEST_COMPILATION_NAME].kotlinSourceSets.first())
+            }
     }
 
     sourceSets {
