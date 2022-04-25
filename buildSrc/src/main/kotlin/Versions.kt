a8d194a2bfc428ee11c6e9de2d86f72168e0d71a
Him188 <Him188@mamoe.net>
2022-04-25 16:20:49 +0100

    Set project version 0.3.0-162.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 @file:Suppress("MemberVisibilityCanBePrivate")
 
 object Versions {
-    const val project = "0.2.0-162.1"
+    const val project = "0.3.0-162.1"
     const val idePlugin = "221-$project-1"
 
     const val intellij = "2022.1"
