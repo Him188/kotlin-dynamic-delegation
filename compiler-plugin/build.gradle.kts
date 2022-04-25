7d5bf3153bdeee8d9a2497f383bd36e309e2b4f8
Him188 <Him188@mamoe.net>
2022-04-25 14:29:11 +0100

    Bump dependencies

--- compiler-plugin/build.gradle.kts
+++ compiler-plugin/build.gradle.kts
@@ -13,8 +13,8 @@ dependencies includeInShadow@{
 dependencies compileOnly@{
     compileOnly(kotlin("stdlib")) // don't include stdlib in shadow
     compileOnly("org.jetbrains.kotlin:kotlin-compiler:${Versions.kotlin}")
-    kapt("com.google.auto.service:auto-service:1.0")
-    compileOnly("com.google.auto.service:auto-service-annotations:1.0")
+    kapt("com.google.auto.service:auto-service:1.0.1")
+    compileOnly("com.google.auto.service:auto-service-annotations:1.0.1")
 }
 
 dependencies tests@{
@@ -28,15 +28,18 @@ dependencies tests@{
 
     testImplementation(kotlin("test-junit5"))
 
-    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.5")
-    testImplementation("org.assertj:assertj-core:3.21.0")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.8")
+    testImplementation("org.assertj:assertj-core:3.22.0")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
-embeddableCompiler() {
+embeddableCompiler {
     archiveFileName.set("${project.name}-all.jar")
+}
+repositories {
+    mavenCentral()
 }
 
 val test by tasks.getting(Test::class) {
