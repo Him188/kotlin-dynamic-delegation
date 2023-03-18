31372e2d8494b1efc214458682f29447cc90539c
Him188 <Him188@mamoe.net>
2023-03-18 17:30:26 +0000

    Do not automatically register runtime dependencies

--- gradle-plugin/src/main/kotlin/CompilerGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/CompilerGradlePlugin.kt
@@ -4,9 +4,6 @@ import me.him188.kotlin.dynamic.delegation.build.BuildConfig
 import me.him188.kotlin.dynamic.delegation.compiler.DynamicDelegationCommandLineProcessor
 import org.gradle.api.Project
 import org.gradle.api.provider.Provider
-import org.jetbrains.kotlin.gradle.dsl.KotlinJsProjectExtension
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.*
 
 
@@ -25,46 +22,6 @@ private val pluginArtifact = SubpluginArtifact(
 @Suppress("unused") // used in build.gradle.kts
 public open class CompilerGradlePlugin : KotlinCompilerPluginSupportPlugin {
     override fun apply(target: Project) {
-
-        kotlin.runCatching { target.extensions.getByType(KotlinMultiplatformExtension::class.java) }
-            .fold(onSuccess = { kotlin ->
-                // when MPP
-
-                val applicableTargets =
-                    kotlin.targets.filter { it.platformType == KotlinPlatformType.common }
-
-                for (applicableTarget in applicableTargets) {
-                    applicableTarget.compilations.flatMap { it.allKotlinSourceSets }.forEach {
-                        it.dependencies {
-                            implementation("${BuildConfig.GROUP_ID}:${BuildConfig.ARTIFACT_ID_RUNTIME}:${BuildConfig.VERSION}")
-                        }
-                    }
-                }
-                if (applicableTargets.isNotEmpty()) {
-                    target.repositories.mavenCentral()
-                }
-
-            }, onFailure = {
-                if (kotlin.runCatching { target.extensions.getByType(KotlinJvmProjectExtension::class.java) }.isSuccess) {
-                    // when JVM
-                    target.dependencies.add(
-                        "compileOnly",
-                        "${BuildConfig.GROUP_ID}:${BuildConfig.ARTIFACT_ID_RUNTIME}:${BuildConfig.VERSION}"
-                    )
-                    target.repositories.mavenCentral()
-                }
-
-                if (kotlin.runCatching { target.extensions.getByType(KotlinJsProjectExtension::class.java) }.isSuccess) {
-                    // when JS
-                    target.dependencies.add(
-                        "compileOnly",
-                        "${BuildConfig.GROUP_ID}:${BuildConfig.ARTIFACT_ID_RUNTIME}:${BuildConfig.VERSION}"
-                    )
-                    target.repositories.mavenCentral()
-                }
-            })
-
-//        target.extensions.create("myExtension", MyExtension::class.java)
     }
 
     override fun getCompilerPluginId(): String = DynamicDelegationCommandLineProcessor.COMPILER_PLUGIN_ID
